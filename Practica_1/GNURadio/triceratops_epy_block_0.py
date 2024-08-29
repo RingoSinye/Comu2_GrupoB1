3866dea0ca015f1a22230c7f72799897fdc067a0
comu2_grupob1 <comu2_grupob1@labcom-Profe>
2024-08-29 08:23:49 -0500

    save

---
+++ Practica_1/GNURadio/triceratops_epy_block_0.py
@@ -0,0 +1,28 @@
+"""
+Embedded Python Blocks:
+
+Each time this file is saved, GRC will instantiate the first class it finds
+to get ports and parameters of your block. The arguments to __init__  will
+be the parameters. All of them are required to have default values!
+"""
+
+import numpy as np
+from gnuradio import gr
+
+class blk (gr.sync_block):
+
+	def __init__(self) : # only default arguments here
+		gr.sync_block.__init__(
+		self ,
+		name ='Acumulador', # will show up in GRC
+		in_sig = [np.float32],
+		out_sig = [np.float32]
+		)
+		
+	def work (self , input_items , output_items ):
+
+		x = input_items[0] # Senal de entrada .
+		y0 = output_items[0] # Senal acumulada
+		limit= x[:2]
+		y0[:] = np.cumsum(limit)
+		return len(y0)
